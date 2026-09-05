20fb9082e655d4adcaa8ccd74392d23d1c366586
tororsteen <tororsteen@gmail.com>
2026-09-05 11:22:45 +0200

    Oppdatert 050926

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,20 @@
+return{
+{
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    -- Only one of these is needed.
+    "sindrets/diffview.nvim",        -- optional
+
+    -- For a custom log pager
+    "m00qek/baleia.nvim",            -- optional
+
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gi", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+  }
+}
+}
